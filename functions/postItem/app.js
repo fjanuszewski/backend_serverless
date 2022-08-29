dedc69faf7bb78e25e3d0f890c33a00611a72c06
Fabian L. Januszewski <fabian.januszewski@gmail.com>
2022-08-29 19:27:46 -0300

    add: fix sdk x-ray and type data on DynamoDB

--- functions/postItem/app.js
+++ functions/postItem/app.js
@@ -1,33 +1,4 @@
-// const AWSXRay = require('aws-xray-sdk')
-// const AWS = AWSXRay.captureAWS(require('aws-sdk'))
-
-// const documentClient = new AWS.DynamoDB.DocumentClient();
-
-// const responseFactory = (error, result) => ({
-//   statusCode: (error) ? error.response.status : 200,
-//   headers: { 'Access-Control-Allow-Origin': '*' },
-//   body: (error) ? JSON.stringify(error.response.data) : JSON.stringify(result)
-// });
-
-// const createItem = (idItem, description) => {
-//   return new Promise((resolve, reject) => {
-//     const params = {
-//       TableName: process.env.EXAMPLE_TABLE,
-//       Item: {
-//         idItem,
-//         description
-//       }
-//     };
-//     documentClient.put(params, (err, data) => {
-//       if (err)
-//         return reject(err);
-//       resolve(params.Item);
-//     });
-//   });
-// }
-
-
-const AWSXRay = require('aws-xray-sdk')
+const AWSXRay = require('aws-xray-sdk') 
 const {responseHandler} = require('/opt/nodejs/commons')
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocumentClient, PutCommand } = require('@aws-sdk/lib-dynamodb');
@@ -55,13 +26,18 @@ exports.handler = async (event) => {
   try {
     const item = JSON.parse(event.body);
 
+    AWSXRay.captureFunc('annotations', function(subsegment) {
+      subsegment.addAnnotation('idItem', item.idItem);
+    });
+
     if (!item.description && !item.idItem)
       return responseHandler({ response: { status: 400, data: { message: 'Invalid request body. Missing description data.' }}});
-
+    
+    item.idItem = item.idItem.toString()
     const res = await createItem(item);
     return responseHandler(null, res);
   } catch (error) {
-    console.log(error);
+    console.error("ERROR: ",error);
     return responseHandler({
       response: error.response || { status: 500, data: { message: 'Internal server error.' } }
     });
