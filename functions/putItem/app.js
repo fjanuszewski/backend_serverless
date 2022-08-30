aac5997ee1eee50140e592d279a5f717264602ea
Fabian L. Januszewski <fabian.januszewski@gmail.com>
2022-08-30 16:31:00 -0300

    feat: add put

--- functions/putItem/app.js
+++ functions/putItem/app.js
@@ -1,16 +1,22 @@
 const AWSXRay = require('aws-xray-sdk')
 const {responseHandler} = require('/opt/nodejs/commons')
 const {DynamoDBClient} = require('@aws-sdk/client-dynamodb');
-const {DynamoDBDocumentClient,PutCommand} = require('@aws-sdk/lib-dynamodb');
+const {DynamoDBDocumentClient,UpdateCommand} = require('@aws-sdk/lib-dynamodb');
 
 const updateItem = async (item) => {
   const dynamoDbClient = AWSXRay.captureAWSv3Client(new DynamoDBClient({}))
   const dynamodbDocumentClient = DynamoDBDocumentClient.from(dynamoDbClient);
   try {
     const result = await dynamodbDocumentClient.send(
-      new PutCommand({
+      new UpdateCommand({
         TableName: process.env.EXAMPLE_TABLE,
-        Item: item,
+          Key: {
+          idItem: item.idItem,
+          },
+          UpdateExpression: 'set description = :s_description', // For example, "'set Title = :t, Subtitle = :s'"
+          ExpressionAttributeValues: {
+            ':s_description': item.description,
+          },
         ConditionExpression: 'attribute_exists(idItem)'
       })
     );
@@ -20,20 +26,19 @@ const updateItem = async (item) => {
   }
 };
 
-
 exports.handler = async (event) => {
   console.log('START', JSON.stringify(event));
   try {
-    const { pathParameters: { idItem }, body: description } = event;
+    const item = {idItem: event.pathParameters.idItem,
+                  description: JSON.parse(event.body).description}
 
     AWSXRay.captureFunc('annotations', function (subsegment) {
-      subsegment.addAnnotation('idItem', idItem);
+      subsegment.addAnnotation('idItem', item.idItem);
     });
 
-    if (!description)
+    if (!item.description)
       return responseHandler({ response: { status: 400, data: { message: 'Invalid request body. Missing description data.' } } });
 
-    const item = {idItem,description}
     const res = await updateItem(item);
     return responseHandler(null, res);
   } catch (error) {
